e7cbda1d15cdeaa10bce44f09a2a1e6d9b2006d1
mike.titterington <mike@MikeT-PC.ad.solidstatescientific.com>
2022-11-03 14:35:13 -0400

    sheets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -140,8 +140,8 @@
 		};
 	});
 </script>
-<div class='p-8 max-w-screen max-h-full min-h-screen mx-auto contain dark:bg-gray-800 dark:text-gray-300'>
-	<nav class="bg-gray-600">
+<div class='py-8 max-w-screen max-h-full min-h-screen mx-auto contain dark:bg-gray-800 dark:text-gray-300'>
+	<nav class="bg-gray-600 mx-8">
 		<div class="lg:flex justify-center max-w-8xl mx-auto px-2 sm:px-6 lg:px-8">
 		  <div class="relative flex items-center justify-between h-16">
 			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
--- src/routes/schedules/+page.svelte
+++ src/routes/schedules/+page.svelte
@@ -49,9 +49,9 @@
             <option value="{team[5]}">{team[5]}</option>
         {/each}
     </select>
-    <div class='py-4 w-full grid gap-4 grid-cols-1 p-6 text-center rounded-md shadow-sm flex flex-col items-center overflow-x-auto'>
+    <div class='py-4 w-full grid gap-4 grid-cols-1 py-6 text-center rounded-md shadow-sm flex flex-col items-center overflow-x-auto'>
         {#if filteredTeams.length > 1}
-            <div class="flex flex-col">
+            <div class="flex flex-col" in:fade="{{ duration:1000, ease:'circ' }}">
                 <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="overflow-hidden">
